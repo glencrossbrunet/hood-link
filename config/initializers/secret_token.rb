1e34b32a848572a5ad86ed9154c78ac10f155806
aj0strow <alexander.ostrow@gmail.com>
2013-10-04 16:17:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoodLink::Application.config.secret_key_base = '264488c6a3eda69d84abd65cc1cf22d0562547fbb28fd7920312cb7d0b0946b6c82c8b913fefc191f2fe741f08729acbbfe8bf0fda9ccf0e51c1eb8f1cf65dfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HoodLink::Application.config.session_store :cookie_store, key: '_hood-link_session'
