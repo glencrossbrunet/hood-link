91ae3a93a572640b4226da2fdcdf29d8a74b12cf
aj0strow <alexander.ostrow@gmail.com>
2013-10-07 03:54:23 -0400

    almost done with admin membership part

--- app/assets/javascripts/dashboard/router.js
+++ app/assets/javascripts/dashboard/router.js
@@ -17,17 +17,20 @@ HL.Router = Backbone.Router.extend({
   
   dashboard: function() {
     console.log('route: dashboard');
+    $('a[href="dashboard"]').addClass('selected');
     this.show(new HL.DashboardView());
   },
   
   
   filters: function() {
     console.log('route: filters');
+    $('a[href="filters"]').addClass('selected');
     this.show(new HL.FiltersView());
   },
   
   members: function() {
     console.log('route: members');
+    $('a[href="members"]').addClass('selected');
     this.show(new HL.MembersView({ collection: this.roles }));
   },
 
--- app/assets/javascripts/dashboard/views/navigation.js
+++ app/assets/javascripts/dashboard/views/navigation.js
@@ -4,7 +4,9 @@ HL.NavigationView = Backbone.View.extend({
   },
   
   navigate: function(ev) {
-    ev && ev.preventDefault && ev.preventDefault();
+    ev.preventDefault();
+    this.$('a').removeClass('selected');
+    
     var href = $(ev.target).attr('href');
     Backbone.history.navigate(href, true);
   }  
