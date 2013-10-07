574d5c7b319a251263a2255d8869cd3430e071a7
aj0strow <alexander.ostrow@gmail.com>
2013-10-06 21:44:40 -0400

    added cross subdomain cookies for sessions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ HoodLink::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # sessions across subdomains with cookie
+  config.middleware.use 'CustomDomainCookie', '.lvh.me'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,6 @@ HoodLink::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -77,4 +76,8 @@ HoodLink::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  
+  # sessions across subdomains with cookie
+  config.middleware.use 'CustomDomainCookie', '.hood-link.co'
 end
