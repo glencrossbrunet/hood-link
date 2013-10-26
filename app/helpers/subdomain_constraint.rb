6d9977c10b1a6c99138711e93900daa841e1861f
aj0strow <alexander.ostrow@gmail.com>
2013-10-26 18:13:53 -0400

    back to 1 subdomain

--- app/helpers/subdomain_constraint.rb
+++ app/helpers/subdomain_constraint.rb
@@ -2,11 +2,7 @@ module SubdomainConstraint
   
   def matches?(request)
     subdomains = request.subdomains
-    if Rails.env.production?
-      subdomains.length == 2 && Organization.exists?(subdomain: subdomains.first)
-    else
-      subdomains.length == 1 && Organization.exists?(subdomain: subdomains.first)
-    end
+    subdomains.length == 1 && Organization.exists?(subdomain: subdomains.first)
   end
   
   extend self
