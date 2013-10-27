58df27ebb05d3be4addbff981e8d559cacc8b8ea
aj0strow <alexander.ostrow@gmail.com>
2013-10-26 20:33:06 -0400

    added sample average

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -18,4 +18,23 @@ class Sample < ActiveRecord::Base
   belongs_to :sample_metric
   
   validates_presence_of :fume_hood_id, :sample_metric_id, :value, :sampled_at
+  
+  def self.avg(sample_metric, datetime_range)
+    samples = where(sample_metric_id: sample_metric.id)
+    samples = samples.where(sampled_at: datetime_range)
+    samples = samples.order('sampled_at ASC')
+          
+    total_seconds = datetime_range.end.to_i - samples.first.sampled_at.to_i    
+    average = 0.0
+    
+    samples.each_cons(2) do |start, stop|
+      seconds = stop.sampled_at.to_i - start.sampled_at.to_i
+      average += (seconds.to_f / total_seconds) * start.value
+    end
+
+    seconds = datetime_range.end.to_i - samples.last.sampled_at.to_i
+    average += (seconds.to_f / total_seconds) * samples.last.value
+    
+    average
+  end
 end
--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -41,4 +41,44 @@ describe Sample do
     end
   end
   
+  describe '#avg' do
+    let(:sample_metric) { create(:sample_metric) }
+    let(:fume_hood) { create(:fume_hood) }
+    let(:defaults) do
+      { sample_metric_id: sample_metric.id, 
+        fume_hood_id: fume_hood.id }
+    end
+    
+    let(:start) { DateTime.parse('2013-01-01 05:00:00') }
+    
+    before :each do
+      Sample.create defaults.merge(sampled_at: start, value: 1.0)
+      Sample.create defaults.merge(sampled_at: start + 2.hours, value: 2.0)
+    end
+        
+    describe '1 hour in' do
+      let(:offset) { 1.hour }
+      subject { fume_hood.samples.avg(sample_metric, start .. (start + offset)) }
+      it { should be_within(0.001).of(1.0) }
+    end
+    
+    describe '2 hours in' do
+      let(:offset) { 2.hours }
+      subject { fume_hood.samples.avg(sample_metric, start .. (start + offset)) }
+      it { should be_within(0.001).of(1.0) }
+    end
+    
+    describe '3 hours in' do
+      let(:offset) { 3.hours }
+      subject { fume_hood.samples.avg(sample_metric, start .. (start + offset)) }
+      it { should be_within(0.001).of((2 * 1.0 + 1 * 2.0) / 3) }
+    end
+    
+    describe '4 hours in' do
+      let(:offset) { 4.hours }
+      subject { fume_hood.samples.avg(sample_metric, start .. (start + offset)) }
+      it { should be_within(0.001).of((2 * 1.0 + 2 * 2.0) / 4) }
+    end
+  end
+  
 end
